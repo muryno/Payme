27fd5d152e803808e4ed899a474b3142796fc67d
Muraino Yakubu <murainoy@yahoo.com>
2020-05-30 22:36:28 +0100

    scroll view for details

---
+++ Payme/UIController/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Payme
+//
+//  Created by Muraino Yakubu on 5/30/20.
+//  Copyright © 2020 Muraino Yakubu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
